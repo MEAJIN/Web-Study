c5473084d118114c4094b40c67b50ece25169daf
MEAJIN <kimo3o98@gmail.com>
2021-10-28 03:31:39 +0900

    1028

---
+++ 실습 과제/JS/내 노트북 용량.js
@@ -0,0 +1,13 @@
+// 여기에 코드를 입력해 주세요.
+function teraToGiga(giga) {
+ console.log(giga+'TB는');
+ console.log(giga*1024 +'GB 입니다.');
+}
+function teraToMega(mega) {
+ console.log(mega+'TB는');
+ console.log(mega*1024*1024 +'MB 입니다.');
+}
+// TB -> GB 테스트
+teraToGiga(2);
+// TB -> MB 테스트
+teraToMega(2);
---
+++ 실습 과제/JS/자랑스러운 애국가.js
@@ -0,0 +1,26 @@
+// 코드를 작성해 주세요.
+function printChorus(){
+  console.log('무궁화 삼천리 화려 강산');
+  console.log('대한 사람 대한으로 길이 보전하세');
+}
+
+// 애국가 가사
+console.log('1절');
+console.log('동해 물과 백두산이 마르고 닳도록');
+console.log('하느님이 보우하사 우리나라 만세');
+printChorus();
+
+console.log('2절');
+console.log('남산 위에 저 소나무 철갑을 두른 듯');
+console.log('바람서리 불변함은 우리 기상일세');
+printChorus();
+
+console.log('3절');
+console.log('가을 하늘 공활한데 높고 구름 없이');
+console.log('밝은 달은 우리 가슴 일편단심일세');
+printChorus();
+
+console.log('4절');
+console.log('이 기상과 이 맘으로 충성을 다하여');
+console.log('괴로우나 즐거우나 나라 사랑하세');
+printChorus();
