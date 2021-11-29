62f9f02284402f4d71af43c4cbb0a4c8ccbcb706
MEAJIN <kimo3o98@gmail.com>
2021-11-30 02:18:40 +0900

    1130 코드정리

--- 실습 과제/JS 실습/Slide Down 모달창/index.js
+++ 실습 과제/JS 실습/Slide Down 모달창/index.js
@@ -30,41 +30,5 @@ $('#jumbotron-btn').click(function(){
 
 // 모달창 애니메이션으로 닫기
 $ ('#close').click(function(){
-  $('.modal-box').animate({ marginTop : '-1500px'}, 500).hide();
-});
-
-
-// input 안의 값이 안녕 일때 모달창 활성
-// $('#jumbotron-btn').on('click', function () {
-//   if ($('#test-input').val() == '안녕') {
-//     $('.modal-box').fadeIn()
-//   }
-// });
-
-// nav-서브메뉴 열기/닫기
-$('#slidedown').click(function () {
-  $('.nav-sub').slideToggle()
-});
-
-// 폼을 전송할 때 이메일 input안의 값이 공백인 경우, 전송을 막는 기능을 추가
-$('form').on('submit', function (e) {
-  if ($('#email').val() == '') {
-    e.preventDefault();
-    $('#email-alert').show();
-  }
-
-  if ($('#pw').val() == '') {
-    e.preventDefault();
-    $('#pw-alert').show();
-  }
-});
-
-// 사이드 메뉴 열기
-$('#show-Menu').click(function(){
-  $('.left-menu').animate({ marginLeft : '0px'}, 500);
-});
-
-// 사이드 메뉴 닫기
-$('#menu-close').click(function(){
-  $('.left-menu').animate({ marginLeft : '-300'}, 500);
+  $('.modal-box').animate({ marginTop : '-1500px'}, 500);
 });
