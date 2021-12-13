75d029f5aaa72ccbae7bda01f6f9d9eb975263c1
MEAJIN <kimo3o98@gmail.com>
2021-12-13 23:23:17 +0900

    1213

---
+++ 실습 과제/JS 실습/인터랙티브 form 만들기/index.js
@@ -0,0 +1,26 @@
+// jq
+// $('#option1').on('change', function(){
+
+//   let choose = $('#option1').val();
+
+//   if ( choose == '셔츠'){
+//     $('.size-select').show();
+//   } else {
+//     $('.size-select').hide();
+//   }
+
+// });
+
+// js
+document.getElementById('option1').addEventListener('click', function () {
+
+  let userChoose = document.getElementById('option1').value;
+  let element = document.getElementById('size-select');
+
+  if (userChoose == '셔츠') {
+    element.style.display = 'block';
+  } else {
+    element.style.display = 'none';
+  }
+
+});
