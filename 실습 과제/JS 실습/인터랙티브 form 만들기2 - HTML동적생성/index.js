5fa0efa48bd535c4230d6323da2a1b5fd30ca6e2
MEAJIN <kimo3o98@gmail.com>
2021-12-16 23:51:49 +0900

    1216 js코드 추가 중

--- 실습 과제/JS 실습/인터랙티브 form 만들기2 - HTML동적생성/index.js
+++ 실습 과제/JS 실습/인터랙티브 form 만들기2 - HTML동적생성/index.js
@@ -1,38 +1,49 @@
 // jq
-$('#option1').on('change', function(){
+// $('#option1').on('change', function(){
 
-  let choose = $('#option1').val();
+//   let choose = $('#option1').val();
+//   let htmlClear = $('#option2').html(' ');
+  
+//   if ( choose == '셔츠'){
+//     htmlClear;
 
-  if ( choose == '셔츠'){
-    let 템플릿 = `
-    <option>95</option>
-    <option>100</option>
-    <option>105</option>`;
+//     let goodsSizeList = `
+//     <option>95</option>
+//     <option>100</option>
+//     <option>105</option>`;
     
-    $('#option2').html(' ');
-    $('#option2').append(템플릿);
-  } else {
-    let 템플릿 = `
-    <option>26</option>
-    <option>27</option>
-    <option>28</option>`;
+//     $('#option2').append(goodsSizeList);
+//   } else if (choose == '바지'){
+//     htmlClear;
+
+//     let goodsSizeList = `
+//     <option>26</option>
+//     <option>27</option>
+//     <option>28</option>`;
     
-    $('#option2').html(' ');
-    $('#option2').append(템플릿);
-  }
-
-});
-
-// js
-// document.getElementById('option1').onchange = function () {
-
-//   let userChoose = document.getElementById('option1').value;
-//   let element = document.getElementById('size-select');
-
-//   if (userChoose == '셔츠') {
-//     element.style.display = 'block';
+//     $('#option2').append(goodsSizeList);
 //   } else {
-//     element.style.display = 'none';
+//     htmlClear;
 //   }
 
-// };
+// });
+
+// js
+document.getElementById('option1').onchange = function () {
+
+  let userChoose = document.getElementById('option1').value;
+  let htmlClear = document.getElementById('option2').innerHTML = ' ';
+  
+
+  if (userChoose == '셔츠') {
+    let element = document.createElement('option');
+
+    let goodsSizeList = `10 95 100`;
+
+    element.append(goodsSizeList);
+  } else {
+    
+
+}
+
+};
