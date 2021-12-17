1ba476ed8ba6644aba62e983bcdb51d2d26c9cbd
MEAJIN <kimo3o98@gmail.com>
2021-12-17 23:37:25 +0900

    1217 dom 코드 추가

--- 실습 과제/JS 실습/인터랙티브 form 만들기2 - HTML동적생성/index.js
+++ 실습 과제/JS 실습/인터랙티브 form 만들기2 - HTML동적생성/index.js
@@ -3,7 +3,7 @@
 
 //   let choose = $('#option1').val();
 //   let htmlClear = $('#option2').html(' ');
-  
+
 //   if ( choose == '셔츠'){
 //     htmlClear;
 
@@ -11,7 +11,7 @@
 //     <option>95</option>
 //     <option>100</option>
 //     <option>105</option>`;
-    
+
 //     $('#option2').append(goodsSizeList);
 //   } else if (choose == '바지'){
 //     htmlClear;
@@ -20,7 +20,7 @@
 //     <option>26</option>
 //     <option>27</option>
 //     <option>28</option>`;
-    
+
 //     $('#option2').append(goodsSizeList);
 //   } else {
 //     htmlClear;
@@ -28,22 +28,55 @@
 
 // });
 
-// js
+// dom
 document.getElementById('option1').onchange = function () {
 
-  let userChoose = document.getElementById('option1').value;
-  let htmlClear = document.getElementById('option2').innerHTML = ' ';
-  
+  // DocumentFragment 노드 생성
+  let $fragment = document.createDocumentFragment();
 
-  if (userChoose == '셔츠') {
-    let element = document.createElement('option');
+  let $option1 = document.getElementById('option1');
+  let $option2 = document.getElementById('option2');
+  const clearing = $option2.textContent = '';
 
-    let goodsSizeList = `10 95 100`;
+  if ($option1.value == '셔츠') {
 
-    element.append(goodsSizeList);
+    // 0. $option2 요소의 모든 자식을 단일 #text 노드로 대체 (innerHTML 보다 빠름)
+    clearing;
+
+    ['95', '100', '105'].forEach(text => {
+
+      // 1. 요소 노드 생성
+      let $option = document.createElement('option');
+
+      // 2. 텍스트 노드 생성
+      let textnode = document.createTextNode(text);
+
+      // 3. 텍스트 노드를 $option 요소 노드의 자식으로 추가
+      $option.appendChild(textnode);
+
+      // 4. $option 요소 노드를 DocumentFragment 노드의 마지막 자식 노드로 추가
+      $fragment.appendChild($option)
+
+      // 5. DocumentFragment 노드를 $option 요소 노드의 마지막 자식 노드로 추가
+      $option2.appendChild($fragment);
+    });
+  } else if ($option1.value == '바지') {
+
+    clearing;
+
+    ['26', '27', '28'].forEach(text => {
+
+      let $option = document.createElement('option');
+
+      let textnode = document.createTextNode(text);
+
+      $option.appendChild(textnode);
+
+      $fragment.appendChild($option)
+
+      $option2.appendChild($fragment);
+    });
   } else {
-    
-
-}
-
+    clearing;
+  }
 };
