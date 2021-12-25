6789d5a1e33cd6923bfb01b37ec105e457d44bdc
MEAJIN <kimo3o98@gmail.com>
2021-12-25 23:52:35 +0900

    1225 필터 맞짱 뜨는 중

--- JS/index.js
+++ JS/index.js
@@ -18,10 +18,10 @@ let $price = document.getElementsByClassName('price');
 
 // 데이터바인딩
 function set() {
-for (i = 0; i < products.length; i++) {
-  $tilte[i].innerHTML = products[i].title;
-  $price[i].innerHTML = '가격 : ' + products[i].price;
-};
+  for (i = 0; i < products.length; i++) {
+    $tilte[i].innerHTML = products[i].title;
+    $price[i].innerHTML = '가격 : ' + products[i].price;
+  };
 }
 
 // 모든 자식 노드 삭제 함수
@@ -37,29 +37,58 @@ set()
 // 가격순정렬--------------------------------------------
 let $sellBtn = document.getElementById('sell-btn');
 
-$sellBtn.addEventListener('click', function(){
-  products.sort(function(a,b){
+$sellBtn.addEventListener('click', function () {
+  products.sort(function (a, b) {
     return a.price - b.price
   })
-  
+
   set()
 });
 
 // 글자순정렬--------------------------------------------
 let $textBtn = document.getElementById('text-btn');
 
-$textBtn.addEventListener('click', function(){
-  products.sort(function(a,b){
+$textBtn.addEventListener('click', function () {
+  products.sort(function (a, b) {
     return (a.title > b.title) - (a.title < b.title)
   })
   set()
-  });
+});
 
 // -------------------------------------------------- //
 
-let document.getElementById
+let fiveBtn = document.getElementById('five-btn');
+let card = document.getElementsByClassName('card');
 
-// 5만원 이하 상품 정렬
-const fiveWonDown = products.filter(function(a){
-  return a.price <= 50000
-})
+fiveBtn.addEventListener('click', function () {
+
+  // 5만원 이하 상품 필터링
+  let fiveWonDown = products.filter(function (a) {
+    return a.price <= 50000
+  });
+
+
+  for (i = 0; i < products.length; i++) {
+    for (j = 0; j < fiveWonDown.length; j++) {
+      if (fiveWonDown[j].price =! products[i].price) {
+        // #card 싹다 지움
+        for (i = 0; i < card.length; i++) {
+          card[i].style.display = 'none'; 
+        }
+      }
+    }
+  }
+
+});
+
+
+
+
+// function get() {
+//   for (i = 0; i < fiveWonDown.length; i++) {
+//     $tilte[i].innerHTML = fiveWonDown[i].title;
+//     $price[i].innerHTML = '가격 : ' + fiveWonDown[i].price;
+//   };
+// }
+
+// get()
