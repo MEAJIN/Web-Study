ca25526c3f22149dc99588c624548696d90abd6e
MEAJIN <kimo3o98@gmail.com>
2022-02-09 23:46:43 +0900

    0209 검색 기능 구현 중

--- 프로젝트/웹 개발 기능대회 예제/index.js
+++ 프로젝트/웹 개발 기능대회 예제/index.js
@@ -1,3 +1,7 @@
+///////////////////////
+// 첫 로딩의 시작 점 //
+//////////////////////
+
 $(document).ready(function () {
   첫로드시실행할거();
 });
@@ -8,7 +12,13 @@ function 첫로드시실행할거() {
 
   //데이터 가져와서 박아넣는기능 
   getProducts();
+  search();
 }
+
+
+///////////////////
+// 데이터 바인딩 //
+//////////////////
 
 //json 데이터 ajax로 가져오고 데이터바인딩
 function getProducts() {
@@ -37,15 +47,34 @@ function appendProduct(product, index) {
   $('#product-list').append(newItem);
 };
 
-
+///////////////
 // 검색 기능 //
+//////////////
+
+// 각 클래스를 변수에 저장
+function search() {
+  this.keyword = document.querySelector('input[name = "search"]');
+  this.button = document.querySelector('.btn');
+  this.form = document.querySelector('.search');
+  engine();
+}
+
+//
+function engine() {
+  this.form.addEventListener('submit', e => {
+    e.preventDefault(); // form태그 기본 작동 제한 (서버로데이터전송)
+    let keyword = this.keyword.value;
+    console.log(keyword);
+  })
+}
+
 
 // 검색창에 마우스 올리면 테투리 생성
-$("#search").mouseenter(function () {
-  $('#search').css('box-shadow', '0 0 3px 3px rgba(62, 123, 216, 0.61)');
+$(".keyword").mouseenter(function () {
+  $('.keyword').css('box-shadow', '0 0 3px 3px rgb(143, 188, 255)');
 });
 
 // 마우스가 검색창을 벗어나면 테투리 없앰
-$("#search").mouseleave(function () {
-  $('#search').css('box-shadow', 'none');
+$(".keyword").mouseleave(function () {
+  $('.keyword').css('box-shadow', 'none');
 });
